3540d45d1cef00cbfe27d256fe95b300e5c87e1d
anatawa12 <anatawa12@icloud.com>
2021-05-29 21:14:25 +0900

    implement

--- src/main/java/com/anatawa12/mcAutoCloser/McAutoCloser.java
+++ src/main/java/com/anatawa12/mcAutoCloser/McAutoCloser.java
@@ -1,17 +1,65 @@
 package com.anatawa12.mcAutoCloser;
 
-import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.event.FMLServerStartedEvent;
+import cpw.mods.fml.common.network.NetworkCheckHandler;
+import cpw.mods.fml.relauncher.Side;
+import net.minecraft.command.ICommandSender;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.IChatComponent;
+import net.minecraft.world.World;
+
+import java.util.Map;
 
 @Mod(modid = "server-auto-closer")
-public class McAutoCloser
-{
+@SuppressWarnings("unused")
+public class McAutoCloser implements ICommandSender {
+    // single side mod
+    @NetworkCheckHandler
+    public boolean checkRemote(Map<?, ?> _map, Side _side) {
+        return true;
+    }
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    public void init(FMLServerStartedEvent _event) {
+        MinecraftServer instance = FMLCommonHandler.instance().getMinecraftServerInstance();
+        if (instance instanceof DedicatedServer) {
+            ((DedicatedServer) instance).addPendingCommand("stop", this);
+        }
+    }
+
+    @Override
+    public String getCommandSenderName() {
+        return "McAutoCloser";
+    }
+
+    @Override
+    public IChatComponent func_145748_c_() {
+        return new ChatComponentText(getCommandSenderName());
+    }
+
+    @Override
+    public void addChatMessage(IChatComponent component) {
+        System.out.println("McAutoCloser: " + component);
+    }
+
+    @Override
+    public boolean canCommandSenderUseCommand(int p_70003_1_, String p_70003_2_) {
+        return true;
+    }
+
+    @Override
+    public ChunkCoordinates getPlayerCoordinates() {
+        return new ChunkCoordinates(0, 0, 0);
+    }
+
+    @Override
+    public World getEntityWorld() {
+        return FMLCommonHandler.instance().getMinecraftServerInstance().worldServers[0];
     }
 }
