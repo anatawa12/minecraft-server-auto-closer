c6f316b9029f272ae26a1b5a2d4bcc5c43f5864e
anatawa12 <anatawa12@icloud.com>
2021-05-29 21:07:22 +0900

    initial commit for minecraft-server-auto-closer

--- src/main/java/com/anatawa12/mcAutoCloser/McAutoCloser.java
+++ src/main/java/com/anatawa12/mcAutoCloser/McAutoCloser.java
@@ -1,16 +1,13 @@
-package com.example.examplemod;
+package com.anatawa12.mcAutoCloser;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = "server-auto-closer")
+public class McAutoCloser
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
